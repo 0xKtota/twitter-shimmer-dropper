3818479eabffc101a76754ecb3835eba7dcaef1e
Antonio Nardella <antonio.nardella@iota.org>
2022-10-30 15:45:50 +0100

    rollback

--- main.py
+++ main.py
@@ -231,12 +231,12 @@ def ShowConfigurationTwitterBot():
 
 # Twitter Bot
 def CreateApi():
-    api_key = os.getenv("API_KEY")
-    api_key_secret = os.getenv("API_KEY_SECRET")
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
     access_token = os.getenv("ACCESS_TOKEN")
     access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True)
     try:
